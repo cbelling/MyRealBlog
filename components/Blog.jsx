1176572843485963c3ad172f371e9c84435b3e65
cbelling <charlesbellinger@outlook.com>
2022-07-20 20:06:27 -0400

    updates

--- components/Blog.jsx
+++ components/Blog.jsx
@@ -1,16 +1,9 @@
 import Image from 'next/image';
 import React from 'react';
 import Html from '../public/assets/skills/html.png';
-import Css from '../public/assets/skills/css.png';
-import Javascript from '../public/assets/skills/javascript.png';
-import ReactImg from '../public/assets/skills/react.png';
-import Tailwind from '../public/assets/skills/tailwind.png';
-import Github from '../public/assets/skills/github1.png';
-import Firebase from '../public/assets/skills/firebase.png';
-import NextJS from '../public/assets/skills/nextjs.png'
-import AWS from '../public/assets/skills/aws.png';
+import Link from 'next/link';
 
-const Skills = () => {
+const Blog = () => {
   return (
     <div id='skills' className='w-full p-2'>
       <div className='max-w-[1240px] mx-auto flex flex-col justify-center h-full'>
@@ -19,33 +12,23 @@ const Skills = () => {
         </p>
         <h2 className='py-4'>Read My Stuff</h2>
         <div className='gap-8'>
-          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-              <div className='m-auto'>
+          <Link href='/post1'>
+          <div className='p-3 shadow-xl rounded-xl hover:scale-105 ease-in duration-300 cursor-pointer'>
+            <div className='flex flex-row'>
+              <div className='m-10 left-0'>
                 <Image src={Html} width='64px' height='64px' alt='/' />
               </div>
               <div className='flex flex-col justify-center'>
-                <h3>Blog Entry 1</h3>
-                <p>Description</p>
+                <h3>Monte Carlo Simulations</h3>
+                <p>An introduction to Monte Carlo Simulations</p>
               </div>
             </div>
           </div>
-          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-              <div className='m-auto'>
-                <Image src={Html} width='64px' height='64px' alt='/' />
-              </div>
-              <div className='flex flex-col justify-center'>
-                <h3>Blog Entry 1</h3>
-                <p>Description</p>
-              </div>
-            </div>
-          </div>
-          
+          </Link>
         </div>
       </div>
     </div>
   );
 };
 
-export default Skills;
+export default Blog;
---
+++ components/BlogPost.jsx
@@ -0,0 +1,37 @@
+import Image from 'next/image';
+import React from 'react';
+import Html from '../public/assets/skills/html.png';
+import Link from 'next/link';
+
+const BlogPost = () => {
+  return (
+    <div id='about' className='w-full p-2 flex items-center py-16'>
+        <div className='max-w-[1240px] m-10'>
+            <div className=''>
+                <div className='mb-10'>
+                    <div className='mb-10'>
+                        <p className='uppercase text-xl tracking-widest text-[#49f574]'>
+                        Monte Carlo Simulations
+                        </p>
+                        <p className='italic'>An Introduction</p>
+                    </div>
+                    <div className='drop-shadow-2xl p-5'>
+                        <Image src="/assets/MonteCarlo.png" width='1000px' height='500px' alt='/' />
+                    </div>
+                </div>
+                <p className='py-2 text-gray-600 font-bold'>Introduction</p>
+                <p className='py-2 text-gray-600'>
+                I have spent the last 12 years in the fire service working as a
+                professional firefighter/paramedic. I have always had a knack for
+                technology and working with computers. In 2019 I started working
+                with HTML and CSS to make some minor edits on a small business
+                website that I was operating. What I thought was just a few small
+                edits turned into a love for programming.
+                </p>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default BlogPost;
